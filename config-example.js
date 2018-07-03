922a2d8d53f350c0f03d7c6c037d8e619827cdf8
Chris Troutner <chris.troutner@gmail.com>
2018-07-03 12:29:13 -0700

    Adding config example file

---
+++ config-example.js
@@ -0,0 +1,13 @@
+/*
+  Configuration values should be put here.
+  Rename this file to config.js.
+*/
+
+"use strict";
+
+module.exports = {
+  consumer_key: "",
+  consumer_secret: "",
+  access_token: "",
+  access_token_secret: "",
+};
